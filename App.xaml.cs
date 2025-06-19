e1e4a392cdae5263f348c3754519fd9126ce2b29
Timny Li <126785544+swordsounds@users.noreply.github.com>
2025-06-19 20:53:50 -0230

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -29,9 +29,9 @@ namespace DisplayAnOfflineMapOnDemand
 
             // !!MUST OMIT THESE TO NOT LEAK API KEY!!
             // Set the access token for ArcGIS Maps SDK for .NET.
-            ArcGISRuntimeEnvironment.ApiKey = "AAPTxy8BH1VEsoebNVZXo8HurHI84bxf92iHEtus-iAVKYwnOINb0WgUe2LJCJ93o2bG7BEgy8fEPJYT3-nvNDRkHw_Wzd5_Fk6cnyfCtV2urz9sCeSPjbXIzUl8k2WWoUpzzBYSZIROGYA1wmx8n56Qibln1ix1y6tQo30GArZLDfe8Krer5Wii95knAe65wFXEDZv4AEAhM-oLhAbE1UMB-W2MfW7eUAPG3MTLpnNAwQw.AT1_B4pdHfJs";
+            ArcGISRuntimeEnvironment.ApiKey = ;
             // Set license key
-            string licenseKey = "runtimelite,1000,rud2388348839,none,TRB3LNBHPFK7003AD123";
+            string licenseKey = ;
             ArcGISRuntimeEnvironment.SetLicense(licenseKey);
 
             // Call a function to set up the AuthenticationManager for OAuth.
